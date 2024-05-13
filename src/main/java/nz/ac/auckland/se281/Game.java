2450ec0136af8b57063f8bdebb1f28df1c7bf9c0
gvi744 <jchi648@aucklanduni.ac.nz>
2024-05-14 11:28:41 +1200

    Added functionality for round numbers upon play command

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,11 +6,16 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private Integer roundNumber = 0;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
-    MessageCli.WELCOME_PLAYER.printMessage(options[0]);  
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    roundNumber = 1;
   }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(Integer.toString(roundNumber));
+  }
 
   public void endGame() {}
 
